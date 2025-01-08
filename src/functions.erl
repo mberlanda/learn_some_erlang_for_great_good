71e86956bb28d6a51e0cf6bc10364df7fe545635
Mauro Berlanda <mauro.berlanda@gmail.com>
2025-01-08 22:34:24 +0100

    syntax in functions:guards

--- src/functions.erl
+++ src/functions.erl
@@ -1,5 +1,9 @@
 -module(functions).
--export([greet/2, head/1, second/1, same/2]).
+-export(
+    [
+        greet/2, head/1, second/1, same/2,
+        old_enough/1, right_age/1, wrong_age/1
+    ]).
 
 % https://www.erlang.org/doc/system/typespec.html
 % Pattern matching on Gender atoms
@@ -14,4 +18,15 @@ head([Head | _]) -> Head.
 second([_, Second | _]) -> Second.
 
 same(X, X) -> true;
-same(_,_) -> false.
+same(_, _) -> false.
+
+old_enough(Age) when Age >= 16 -> true;
+old_enough(_) -> false.
+
+% ; is used as a guard separator to define && in Erlang
+right_age(Age) when Age >= 16; Age < 104 -> true;
+right_age(_) -> false.
+
+% , is used as a guard separator to define || in Erlang
+wrong_age(Age) when Age < 16, Age >= 104 -> true;
+wrong_age(_) -> false.
--- test/functions_test.erl
+++ test/functions_test.erl
@@ -32,3 +32,27 @@ same_test() ->
         {"same_undefined", ?_assertError(badarg, functions:same(undefined, 1))},
         {"same_type_mismatch", ?_assertEqual(false, functions:same(1, "1"))}
     ].
+
+% Test for old_enough function
+old_enough_test() ->
+    [
+        {"old_enough_true", ?_assertEqual(true, functions:old_enough(16))},
+        {"old_enough_false", ?_assertEqual(false, functions:old_enough(15))},
+        {"old_enough_undefined", ?_assertError(badarg, functions:old_enough(undefined))}
+    ].
+
+% Test for right_age function
+right_age_test() ->
+    [
+        {"right_age_true", ?_assertEqual(true, functions:right_age(16))},
+        {"right_age_false", ?_assertEqual(false, functions:right_age(15))},
+        {"right_age_undefined", ?_assertError(badarg, functions:right_age(undefined))}
+    ].
+
+% Test for wrong_age function
+wrong_age_test() ->
+    [
+        {"wrong_age_true", ?_assertEqual(true, functions:wrong_age(104))},
+        {"wrong_age_false", ?_assertEqual(false, functions:wrong_age(16))},
+        {"wrong_age_undefined", ?_assertError(badarg, functions:wrong_age(undefined))}
+    ].
